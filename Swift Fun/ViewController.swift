407d82c2f2c398dc8428b92368210580eba2a82e
Paul Smart <paulwsmart@gmail.com>
2018-06-12 22:14:13 -0600

    Our first commit as gitters

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonCount = 0
+    
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func ButtonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        print(buttonCount)
+        
+        if buttonCount >= 10 {
+            view.backgroundColor = UIColor.red
+            myLabel.text = "Paul is cool"
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       
     }
 
     override func didReceiveMemoryWarning() {
